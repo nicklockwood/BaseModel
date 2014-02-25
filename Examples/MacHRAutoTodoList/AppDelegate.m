2ad47fe0b49890cb74af32c9e1a6c6fce1a3ccfa
Alex Gray <alex@mrgray.com>
2014-02-24 20:23:15 -0500

    Cleaned up Mac Demo, reducing SLOC, clarifying, etc. Attempting to suash.

--- Examples/MacHRAutoTodoList/AppDelegate.m
+++ Examples/MacHRAutoTodoList/AppDelegate.m
@@ -1,73 +1,52 @@
-//
-//  AppDelegate.m
-//  MacHRAutoTodoList
-//
-//  Created by Alex Gray on 11/26/12.
-//  Copyright (c) 2012 Alex Gray. All rights reserved.
-//
+
+//  AppDelegate.m   MacHRAutoTodoList
+//  Created by Alex Gray on 11/26/12.  Copyright (c) 2012 Alex Gray. All rights reserved.
 
 #import "AppDelegate.h"
 
-@interface TodoColorCell : NSActionCell
-@end
-@interface TodoPriorityClickCell : NSActionCell
-@end
+@implementation      AppDelegate @synthesize todos = _todos;
 
-@implementation AppDelegate
+- (TodoList*) todos               { return _todos = _todos ?: TodoList.sharedInstance; }
+-  (IBAction) deleteAll:    (id)s { _todos.items = NSMutableArray.new; }
+-  (IBAction) saveTodos:    (id)s { [TodoList.sharedInstance save]; }
+-  (IBAction) copyTodo:     (id)s {	_table.selectedRowIndexes.count ? [self.todos copyTodo:self.todos.items[_table.selectedRow]] : nil; }
+-  (IBAction) newTodo:      (id)s { [_todos newTodo]; }
+-  (IBAction) loadFromPlist:(id)s {
 
-- (TodoList*) todos				  { return _todos = _todos ?: TodoList.sharedInstance; }
-
-- (IBAction) deleteAll: 	(id)s { self.todos.items = [NSMutableArray new];			 }
-
-- (IBAction) saveTodos: 	(id)s { [TodoList.sharedInstance save]; 					 }
-
-- (IBAction) copyTodo:  	(id)s {	_table.selectedRowIndexes.count != 0 ? [self.todos copyTodo:self.todos.items[_table.selectedRow]] : nil; }
-
-- (IBAction) newTodo:   	(id)s { [self.todos newTodo]; }
-
-- (IBAction) loadFromPlist:	(id)s
-{
-	self.todos = [TodoList instanceWithContentsOfFile:TodoList.resourceFile];
-	TodoList.sharedInstance = _todos;
+  [self setValue:TodoList.sharedInstance = [TodoList instanceWithContentsOfFile:TodoList.resourceFile]
+          forKey:@"todos"];
 }
+@end
 
--(void)awakeFromNib
-{
+
+/**  Custom cells, etc...  not directly related to Basemodel, etc... but you can see some ways to access the shared instance, etc */
+
+@interface         TodoColorCell : NSActionCell @end
+@interface TodoPriorityClickCell : NSActionCell @end
+@implementation      AppDelegate   (General)
+- (void) awakeFromNib              {
 	((NSTableColumn*)_table.tableColumns[[_table columnWithIdentifier:   @"Status"]]).dataCell = TodoColorCell.new;
 	((NSTableColumn*)_table.tableColumns[[_table columnWithIdentifier: @"Priority"]]).dataCell = TodoPriorityClickCell.new;
-}
-
-@end
-
-/**  Custom cells...  not directly related to Basemodel, etc... but you can see some ways to access the shared instance, etc */
+}        @end
 
 @implementation TodoPriorityClickCell
+-   (id) target       { return self; }
+-  (SEL) action       { return @selector(tickPriority); }
+- (void) tickPriority {
 
-- (id) target 	{ return self; 					  }
-
-- (SEL) action	{ return @selector( tickPriority ); }
-
-- (void) tickPriority
-{
 	NSUInteger val = [self.objectValue unsignedIntegerValue];
-	((TodoItem*)TodoList.sharedInstance.items[((NSTableView*)self.controlView).selectedRow]).priority = @( val < 8 ? val + 1 : 0 );
+	((TodoItem*)TodoList.sharedInstance.items[((NSTableView*)self.controlView).selectedRow]).priority = @(val < 8 ? val + 1 : 0 );
 }
+- (void) drawWithFrame:(NSRect)cellF inView:(NSView*)v {
 
-- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
-{
 	[NSColor.darkGrayColor set];
-	NSRectFill ( cellFrame );
-	NSString	*string = ((NSNumber*)self.objectValue).stringValue;
-	NSDictionary *attrs = @{ NSFontAttributeName : [NSFont fontWithName:@"Lucida Grande Bold" size: cellFrame.size.height - 10], NSForegroundColorAttributeName : NSColor.whiteColor };
-	NSSize   stringSize = [string sizeWithAttributes:attrs];
- 	[string drawInRect: (NSRect) { NSMidX(cellFrame) - stringSize.width / 2, cellFrame.origin.y + 3, stringSize.width, stringSize.height } withAttributes:attrs];
+	NSRectFill(cellF);
+	NSString    * str = ((NSNumber*)self.objectValue).stringValue;
+	NSDictionary *att = @{ NSFontAttributeName : [NSFont fontWithName:@"Lucida Grande Bold" size: cellF.size.height - 10], NSForegroundColorAttributeName : NSColor.whiteColor};
+	NSSize      strSz = [str sizeWithAttributes:att];
+ 	[str drawInRect:(NSRect){NSMidX(cellF) - strSz.width/2, cellF.origin.y + 3, strSz.width, strSz.height} withAttributes:att];
 }
-
 @end
-
 @implementation TodoColorCell
-
-- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {	[(NSColor*)self.objectValue set];	NSRectFill(cellFrame); }
-
+- (void) drawWithFrame:(NSRect)cellF inView:(NSView*)v {	[(NSColor*)self.objectValue set];	NSRectFill(cellF); }
 @end
-
--- Examples/MacHRAutoTodoList/TodoList.m
+++ Examples/MacHRAutoTodoList/TodoList.m
@@ -1,48 +1,45 @@
 
-//  TodoList.m
-//  TodoListExample
-
-//  Created by Alex Gray on 11/26/12.
-//	Part of BaseModel by Nick Lockwood.
-
+//  TodoList.m -  TodoListExample
+//  Created by Alex Gray on 11/26/12. -	Part of BaseModel by Nick Lockwood.
 
 #import "Todos.h"
 
 @implementation TodoList
 
-/*	note: we've not implemented the NSCoding methods or setWithArray/Dictionary, etc
-	because the HRCoder and AutoCoding libraries take care of this for us 	*/
-
-- (TodoItem *)newTodo
+/*!	@note we've not implemented the NSCoding methods or setWithArray/Dictionary, etc
+          because the HRCoder and AutoCoding libraries take care of this for us 	
+*/
+- (TodoItem*) newTodo
 {
 	TodoItem *newOne = TodoItem.instance;
-	[self insertObject:newOne inItemsAtIndex:self.items.count];  // KVO Array insertion trigger.
+	[self insertObject:newOne inItemsAtIndex:_items.count];  // KVO Array insertion trigger.
 	return newOne;
 }
 
-- (TodoItem *)copyTodo:(TodoItem *)todo;
+- (TodoItem*) copyTodo:(TodoItem*)todo
 {
-	TodoItem *newOne = [todo copy];
-	[self insertObject:newOne inItemsAtIndex:self.items.count];
+	TodoItem *newOne = todo.copy;
+	[self insertObject:newOne inItemsAtIndex:_items.count];
 	return newOne;
 }
 
-- (NSUInteger)countOfItems
+/*! Subclass specific KVO Compliant "items" accessors to trigger NSArrayController updates on inserts / removals. */
 {
     return self.items.count;
 }
 
-- (id)objectInItemsAtIndex:(NSUInteger)index
+- (NSUInteger)                             countOfItems               { return self.items.count; }
 {
     return self.items[index];
 }
 
-- (void)removeObjectFromItemsAtIndex:(NSUInteger)index
+-         (id)                     objectInItemsAtIndex:(NSUInteger)i {	return self.items[i];	}
+-       (void)             removeObjectFromItemsAtIndex:(NSUInteger)i {	[self.items removeObjectAtIndex:i];	}
 {
     [self.items removeObjectAtIndex:index];
 }
 
-- (void)insertObject:(TodoItem*)todo inItemsAtIndex:(NSUInteger)index
+-       (void) insertObject:(TodoItem*)t inItemsAtIndex:(NSUInteger)i {	[self.items insertObject:t atIndex:i];	}
 {
     [self.items insertObject:todo atIndex:index];
 }
